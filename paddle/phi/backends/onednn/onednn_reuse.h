a737f631a61bac5d8add45c54041cf0d8040965b
Jacek Czaja <jacek.czaja@intel.com>
2023-02-02 16:25:16 +0100

    - Fix to softmax

--- paddle/phi/backends/onednn/onednn_reuse.h
+++ paddle/phi/backends/onednn/onednn_reuse.h
@@ -795,7 +795,7 @@ class SoftmaxOneDNNHandler
     int rank = x->dims().size() != 0 ? x->dims().size() : 1;
     const int canonical_axis = funcs::CanonicalAxis(axis, rank);
     this->AcquireForwardPrimitiveDescriptor(
-        dnnl::prop_kind::forward_scoring, x->mem_desc(), canonical_axis);
+        dnnl::prop_kind::forward_inference, x->mem_desc(), canonical_axis);
   }
 
   SoftmaxOneDNNHandler(const dnnl::engine onednn_engine,
@@ -810,7 +810,7 @@ class SoftmaxOneDNNHandler
     int rank = out_grad->dims().size() != 0 ? out_grad->dims().size() : 1;
     const int canonical_axis = funcs::CanonicalAxis(axis, rank);
     this->AcquireForwardPrimitiveDescriptor(
-        dnnl::prop_kind::forward_scoring, out->mem_desc(), canonical_axis);
+        dnnl::prop_kind::forward_inference, out->mem_desc(), canonical_axis);
     this->AcquireBackwardPrimitiveDescriptor(
         out_grad->mem_desc(), out->mem_desc(), canonical_axis);
   }
