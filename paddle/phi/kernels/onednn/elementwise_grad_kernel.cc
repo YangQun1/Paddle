1e2d70e02562ff6b2df3654a26918d4ffb3da630
Jacek Czaja <jacek.czaja@intel.com>
2023-02-10 13:29:22 +0100

    - concat potential blind fix

--- paddle/phi/kernels/onednn/elementwise_grad_kernel.cc
+++ paddle/phi/kernels/onednn/elementwise_grad_kernel.cc
@@ -56,7 +56,7 @@ inline void AddSubNonBroadcast(
   auto reorder_p =
       reorder_handler->AcquireReorder(dst_memory, src_memory, reorder_attr);
 
-  std::unordered_map<int, memory> args = {
+  std::unordered_map<int, dnnl::memory> args = {
       {DNNL_ARG_SRC, *src_memory},
       {DNNL_ARG_DST, *dst_memory},
       {DNNL_ARG_ATTR_SCALES | DNNL_ARG_DST, scales_memory}};
