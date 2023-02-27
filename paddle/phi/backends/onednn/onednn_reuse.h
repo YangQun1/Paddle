d573112e3dfc4934e6af081c3b1463426b7a2bf9
Jacek Czaja <jacek.czaja@intel.com>
2023-02-27 17:38:31 +0100

    - Fix to BN hopefully

--- paddle/phi/backends/onednn/onednn_reuse.h
+++ paddle/phi/backends/onednn/onednn_reuse.h
@@ -1370,9 +1370,9 @@ class BatchNormOneDNNHandler
             scale_tz.size()));
 
     auto scale_memory = this->AcquireMemoryFromPrimitive(
-        this->fwd_pd_->weights_desc(), to_void_cast<DenseTensor>(scale));
+        this->fwd_pd_->weights_desc(), to_void_cast<T>(scale->data<T>()));
     auto shift_memory = this->AcquireMemoryFromPrimitive(
-        this->fwd_pd_->weights_desc(), to_void_cast<DenseTensor>(shift));
+        this->fwd_pd_->weights_desc(), to_void_cast<T>(shift->data<T>()));
 
     return std::make_tuple(scale_memory, shift_memory);
   }
